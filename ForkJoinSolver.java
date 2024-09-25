bffa541869d47a113993ae46d2dfddbbd29ffbe5
potaaaat <louise.hasslof@gmail.com>
2024-09-25 14:53:59 +0200

    Update ForkJoinSolver.java ahhhhhhhhhhhhhhhhhhh

--- ForkJoinSolver.java
+++ ForkJoinSolver.java
@@ -2,7 +2,6 @@ package amazed.solver;
 
 import amazed.maze.Maze;
 
-
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,7 +11,6 @@ import java.util.LinkedList;
 import java.util.Set;
 import java.util.Stack;
 import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.atomic.*;
 
 /**
@@ -57,7 +55,7 @@ public class ForkJoinSolver
         this.forkAfter = forkAfter;
         initStructures();
     }
-
+    
     public ForkJoinSolver(Maze maze, int forkAfter, int startNode, ConcurrentSkipListSet<Integer> visited, AtomicBoolean heartFound)
     {
         this(maze);
@@ -72,8 +70,6 @@ public class ForkJoinSolver
     
     protected void initStructures() {
     	visited = new ConcurrentSkipListSet<Integer>();
-    	availablePaths = new ConcurrentLinkedDeque<Integer>(); 
-    	availablePaths.add(start);
     	//atomic boolean to keep track of when we want to terminate all threads when the goal has been found.
     	heartFound = new AtomicBoolean(false); //if a thread has found the goal this value will chnge to true
     	//if true the other threads need to be terminated: TODO the termination function. 
@@ -83,12 +79,10 @@ public class ForkJoinSolver
     }
 
     protected ConcurrentSkipListSet<Integer> visited;
-    protected ConcurrentLinkedDeque<Integer> availablePaths;
     protected int forkAfter = 0;
     protected AtomicBoolean heartFound;
     protected Map<Integer, Integer> predecessor;
     protected Stack<Integer> frontier;
-    
     /**
 
     /**
@@ -108,17 +102,7 @@ public class ForkJoinSolver
     @Override
     public List<Integer> compute()
     {
-    	 	
-    	while(heartFound.get() == false) {
-        	if(availablePaths.size() > 1){//change 1 later to forkafter
-        		int startNodeForNewPlayer = availablePaths.pop();
-	        	ForkJoinSolver fork = new ForkJoinSolver(this.maze, this.forkAfter, startNodeForNewPlayer, this.visited, this.heartFound);
-	        	fork.fork();
-        	}
-        	else {return parallelSearch();}
-	        	
-        }
-  	return null;
+        return parallelSearch();
     }
     
 
@@ -129,19 +113,13 @@ public class ForkJoinSolver
     	//Frontier are the nodes not visited/ next boxes to go into
         frontier.push(start); 			//keep start where we have 1 player and put the start box in frontier
         // as long as not all nodes have been processed
-
         while (!frontier.empty()) { //not all nodes have been explored
             // get the new node to process
-
-        	if(heartFound.get() == true) {
-                return null;}//if heartFound == true, then some other thread has found the heart and therefore we can stop executing.
-        	
             int current = frontier.pop();
             // if current node has a goal
             if (maze.hasGoal(current)) { //add a func to cancel all other threads if goal is found
                 // move player to goal
                 maze.move(player, current);
-                heartFound.set(true);//we have found the heart so now we have to tell the other threads to stop executing!
                 // search finished: reconstruct and return path
                 return pathFromTo(start, current);
             }
@@ -151,31 +129,30 @@ public class ForkJoinSolver
                 maze.move(player, current);
                 // mark node as visited
                 visited.add(current);
-  
                 // for every node nb adjacent to current
+                int i = 0;
                 for (int nb: maze.neighbors(current)) {//need to create threads here
                     // add nb to the nodes to be processed
-                	System.out.print(frontier);
-                    if(frontier.size()  <= 1) {	frontier.push(nb);}//we want to keep one posible path for ourselves, but if we find more than one available path, we give the rest to others.
-                    else {availablePaths.add(nb);}
-                                  
-
-//                    if (frontier.size() > 2) {//insdead of 1 - use forkAfter
-//                    	int startNodeForNewPlayer = frontier.pop();
-//                    	ForkJoinSolver fork = new ForkJoinSolver(this.maze, this.forkAfter, startNodeForNewPlayer, this.visited, this.heartFound);
-//                    	fork.fork();
-//
-//                    }
+                    frontier.push(nb);
+                    i++;
                     // if nb has not been already visited,
                     // nb can be reached from current (i.e., current is nb's predecessor)
-                    if (!visited.contains(nb)) {
-                        predecessor.put(nb, current);}
+                    if (!visited.contains(nb))
+                        predecessor.put(nb, current);
                 }
-                        
-            }
+                if(frontier.size() < 2) {               	
+                }
+                else {                	
+                ForkJoinSolver onePath = new ForkJoinSolver(this.maze, this.forkAfter, frontier.pop(), this.visited, this.heartFound);
+            	onePath.fork();
+            	ForkJoinSolver anotherPath = new ForkJoinSolver(this.maze, this.forkAfter, frontier.pop(), this.visited, this.heartFound);
+            	anotherPath.fork();
+            	
+            	onePath.join();
+            	anotherPath.join();}
                 //if no more nb the thread should be terminated
+            }
         }
-        
         // all nodes explored, no goal found
         return null;
     }
@@ -193,5 +170,4 @@ public class ForkJoinSolver
         Collections.reverse(path);
         return path;
     }
-    
 }
